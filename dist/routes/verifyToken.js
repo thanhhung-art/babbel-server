c6e4674f6d31f4c9a2ae19ca8c884a48d1796727
thanh-hung <thanhhungdeveloper@gmail.com>
2023-03-20 11:00:36 +0700

    add error handler to veriryToken

--- dist/routes/verifyToken.js
+++ dist/routes/verifyToken.js
@@ -6,20 +6,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const verifyToken = (req, res, next) => {
-    const token = req.cookies['authtoken'];
-    const jwt_secret = process.env.JWT_SECRET;
-    let jwt_payload;
-    if (token && jwt_secret) {
-        jwt_payload = jsonwebtoken_1.default.verify(token, jwt_secret);
-        if (jwt_payload) {
-            if (jwt_payload._id === req.headers['authid']) {
-                next();
-                return;
-            }
-            else {
-                return res.status(401).json({ msg: 'you are not authorized!' });
+    try {
+        const token = req.cookies["authtoken"];
+        const jwt_secret = process.env.JWT_SECRET;
+        let jwt_payload;
+        if (token && jwt_secret) {
+            jwt_payload = jsonwebtoken_1.default.verify(token, jwt_secret);
+            if (jwt_payload) {
+                if (jwt_payload._id === req.headers["authid"]) {
+                    next();
+                    return;
+                }
+                else {
+                    return res.status(401).json({ msg: "you are not authorized!" });
+                }
             }
         }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "An error occurred" });
     }
 };
 exports.verifyToken = verifyToken;
--- routes/verifyToken.ts
+++ routes/verifyToken.ts
@@ -1,20 +1,29 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
-import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies['authtoken'];
-  const jwt_secret: any = process.env.JWT_SECRET
-  let jwt_payload: JwtPayload 
-  
-  if (token && jwt_secret) {
-    jwt_payload = jwt.verify(token, jwt_secret) as { _id: string }
-    if (jwt_payload) {
-      if (jwt_payload._id === req.headers['authid']) {
-        next()
-        return
-      } else {
-        return res.status(401).json({ msg: 'you are not authorized!' })
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies["authtoken"];
+    const jwt_secret: any = process.env.JWT_SECRET;
+    let jwt_payload: JwtPayload;
+
+    if (token && jwt_secret) {
+      jwt_payload = jwt.verify(token, jwt_secret) as { _id: string };
+      if (jwt_payload) {
+        if (jwt_payload._id === req.headers["authid"]) {
+          next();
+          return;
+        } else {
+          return res.status(401).json({ msg: "you are not authorized!" });
+        }
       }
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "An error occurred" });
   }
 };
