224d63802685e21634826978a5edf913b7323014
thanh-hung <thanhhungdeveloper@gmail.com>
2023-03-20 18:27:07 +0700

    remove function allowCors

--- dist/index.js
+++ dist/index.js
@@ -76,17 +76,4 @@ io.on("connection", (socket) => {
 httpServer.listen(port, () => {
     console.log("server is running at port " + port);
 });
-const alllowCors = (fn) => (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    return yield fn(req, res);
-});
-module.exports = alllowCors(app);
+module.exports = app;
