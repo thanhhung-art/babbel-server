a5079888950f458dbafe2a8971b850196a51663f
thanhhung <thanhhungdeveloper@gmail.com>
2025-04-23 17:03:26 +0700

    refactor: remove CacheController and update CacheService to use memoryCache directly

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,11 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheService } from './cache.service';
-import CacheController from './cache.controller';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
+import { createKeyv, Keyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomCacheInterceptor } from './CustomCacheInterceptor';
 
+@Global()
 @Module({
-  imports: [],
-  providers: [CacheService],
-  controllers: [CacheController],
-  exports: [CacheService],
+  imports: [
+    NestCacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        stores: [
+          new Keyv({
+            store: new CacheableMemory({
+              ttl: 6000,
+              lruSize: 5000,
+            }),
+          }),
+          createKeyv(configService.get('REDIS_URL', 'redis://localhost:6379')),
+        ],
+      }),
+    }),
+  ],
+  exports: [CacheService, NestCacheModule],
+  providers: [
+    CacheService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CustomCacheInterceptor,
+    },
+  ],
 })
 export class CacheModule {}
--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,47 +1,45 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { Cache } from 'cache-manager';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class CacheService {
-  constructor(
-    @Inject(CACHE_MANAGER) private readonly cacheService: Cache,
-    private readonly prismaService: PrismaService,
-  ) {}
+  constructor(@Inject(CACHE_MANAGER) private readonly memoryCache: Cache) {}
 
   async clearStore(): Promise<void> {
-    await this.cacheService.store.reset();
+    await this.memoryCache.clear();
   }
 
   async clearCacheByKey(key: string): Promise<void> {
-    await this.cacheService.del(key);
+    await this.memoryCache.del(key);
   }
 
   async clearCachesByKeys(keys: string[]): Promise<void> {
     for (const key of keys) {
-      await this.cacheService.del(key);
+      await this.memoryCache.del(key);
     }
   }
 
   async updateCacheByKey(key: string, value: any): Promise<void> {
-    await this.cacheService.set(key, value);
+    await this.memoryCache.set(key, value);
   }
 
   async updateCachesByKeys(
     caches: { key: string; value: any }[],
   ): Promise<void> {
     for (const cache of caches) {
-      await this.cacheService.set(cache.key, cache.value);
+      await this.memoryCache.set(cache.key, cache.value);
     }
   }
 
   async clearRoomCaches(roomId: string): Promise<void> {
     const pattern = `*${roomId}:*`;
-    const keys = await this.cacheService.store.keys(pattern);
+    const redisClient = (this.memoryCache.stores[0] as any).client;
 
-    if (keys.length > 0) {
-      await this.clearCachesByKeys(keys);
+    if (redisClient?.keys) {
+      const keys = await redisClient.keys(pattern);
+      if (keys.length > 0) {
+        await this.clearCachesByKeys(keys);
+      }
     }
   }
 }
